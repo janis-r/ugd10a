9d5502cddcc4d9eb8b42c81cb352e12269e4db91
Jānis Radiņš <janis.radins@liflandgaming.com>
2018-10-10 12:34:53 +0300

    Package build improved by adding es:next version

--- export-package.ts
+++ export-package.ts
@@ -1,14 +1,16 @@
 const fs = require('fs');
 
 fs.readFile("package.json", {encoding: "utf8"}, (err: Error, data: string) => {
-    const packageData:{[key:string]: any} = JSON.parse(data);
+    if (err) {
+        throw err;
+    }
 
+    const packageData: { [key: string]: any } = JSON.parse(data);
     delete packageData.scripts;
     delete packageData.devDependencies;
 
-    fs.writeFile("dist/package.json", JSON.stringify(packageData, null, 2), {encoding: "utf8"}, (err: Error) => {
+    fs.writeFile("build/package.json", JSON.stringify(packageData, null, 2), {encoding: "utf8"}, (err: Error) => {
         if (err) {
-            console.log(err);
             throw err;
         }
     });
